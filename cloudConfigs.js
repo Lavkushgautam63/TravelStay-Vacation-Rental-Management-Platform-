53d4b4b58eb624ff45d70084fb33324a6b53054e
Lavkush Gautam <lavkush.2201231ec@iiitbh.ac.in>
2026-02-01 03:33:51 +0530

    cloudConfigs.js

--- cloudConfigs.js
+++ cloudConfigs.js
@@ -1,27 +1,23 @@
-// Cloud image storage service import
-const cloudinaryLib = require("cloudinary").v2;
-
-// Multer ke liye Cloudinary storage
+const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-// Cloudinary account config
-cloudinaryLib.config({
-    cloud_name: process.env.CLOUD_NAME,       // cloud name
-    api_key: process.env.CLOUD_API_KEY,       // api key
-    api_secret: process.env.CLOUD_API_SECRET, // api secret
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
-// Multer storage engine (Cloudinary)
-const cloudStorage = new CloudinaryStorage({
-    cloudinary: cloudinaryLib,                // cloudinary instance
-    params: {
-        folder: "wanderlust_DEV",              // upload folder
-        allowedFormats: ["jpg","jpeg","png","svg","ico"], // allowed files
-    },
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "wanderlust_DEV",
+    allowedFormats: ["jpg", "jpeg", "png", "svg", "ico"],
+  },
 });
 
-// Export for use in routes/controllers
 module.exports = {
-    cloudinary: cloudinaryLib, // image delete/update
-    storage: cloudStorage,     // image upload
+  cloudinary,
+  storage,
 };
+
+
